390804aaff9f4eeef7dcaa7b164f16af7bbae211
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-07-20 17:25:03 +0300

    adds a start page

--- src/App.js
+++ src/App.js
@@ -5,18 +5,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>goit-react-hw-01-components</h1>
+        <ul className="nav-list">Навігація по сторінці
+          <li className="nav-bar" id="1-01">
+              <img src={logo} className="App-logo" alt="logo" />
+              Профіль соціальної мережі
+          </li>
+          <li className="nav-bar" id="1-02">
+              <img src={logo} className="App-logo" alt="logo" />
+              Секція статистики
+          </li>
+          <li className="nav-bar" id="1-03">
+              <img src={logo} className="App-logo" alt="logo" />
+              Список друзів
+          </li>
+          <li className="nav-bar" id="1-04">
+              <img src={logo} className="App-logo" alt="logo" />
+              Історія транзакцій
+          </li>
+        </ul>
       </header>
     </div>
   );
