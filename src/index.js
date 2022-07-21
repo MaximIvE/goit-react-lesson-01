41c90b2a19eafe8e34549ba9fcc282e82e8270cc
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-07-21 15:51:03 +0300

    add prop-types

--- src/index.js
+++ src/index.js
@@ -1,63 +1,18 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 import clap from './clap.wav';
-import App from './App';
 
-
-// const elem1 = <span>Привіт </span>;
-// const elem2 = <span>світ</span>;
-
-// const jsxElem = (
-// <div>
-//   {elem1}
-//   {elem2}
-// </div>
-// );
-
-
-// console.log(jsxElem);
-
-
-
-
-
-// ReactDOM.render(
-// <Painting 
-//   url={data.url} 
-//   title={data.title} 
-//   price={data.price} 
-//   authorUrl={data.author.url} 
-//   authorTag={data.author.tag}
-// />, 
-// document.querySelector('#root') );
-
-ReactDOM.render(<App/>, document.querySelector('#root') );
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 var audio = new Audio(clap);
-
 document.onclick = function() {
   audio.volume = 0.08;
   audio.play();
 };
-
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
